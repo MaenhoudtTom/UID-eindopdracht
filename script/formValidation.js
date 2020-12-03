aa05145b40c41b2273a8c30f290c973a4b63f333
MaenhoudtTom <55881682+MaenhoudtTom@users.noreply.github.com>
2020-12-03 18:07:31 +0100

    Update form validation

--- script/formValidation.js
+++ script/formValidation.js
@@ -25,10 +25,14 @@ const getDOMElements = function () {
 
 const enableListeners = function () {
   email.input.addEventListener("blur", function () {
-    if (isEmpty(email.input.value) && !isValidEmailAddress(email.input.value)) {
+    if (isEmpty(email.input.value)) { /* && !isValidEmailAddress(email.input.value) */
       addErrors(email.field, email.errorMessage, "This field is required");
       email.input.addEventListener("input", doubleCheckEmailAdress);
-    } else if (isEmpty(email.input.value)) {
+    }
+    else if (!isValidEmailAddress(email.input.value)) {
+      email.input.addEventListener("input", doubleCheckEmailAdress);
+    } 
+    else if (!isEmpty(email.input.value)) {
       removeErrors(email.field, email.errorMessage);
       email.input.removeEventListener("input", doubleCheckEmailAdress);
     }
@@ -50,9 +54,8 @@ const removeErrors = function (formField, errorField) {
 };
 
 const doubleCheckEmailAdress = function () {
-  if (!isEmpty(email.input.value) && isValidEmailAddress(email.input.value)) {
+  if (isValidEmailAddress(email.input.value)) { /* !isEmpty(email.input.value) && */
     removeErrors(email.field, email.errorMessage);
-    email.input.removeEventListener("input", doubleCheckEmailAdress);
   } else {
     addErrors(email.field, email.errorMessage, "The email is incorrect");
   }
